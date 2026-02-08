707a378a1cc3bec404ddae17c0f7df9cfbcfc8d1
Mr. Rahul Chowdhury <96120918+Rahul-2314@users.noreply.github.com>
2026-02-08 23:57:33 +0530

    Implement findMedian function in find_median.cpp

---
+++ algo/find_median.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    double findMedian(vector<int> &arr) {
+        // code here.
+        int n = arr.size();
+        
+        sort(arr.begin(), arr.end());
+        
+        if(n % 2 == 0){
+            return (arr[n/2 - 1] + arr[n/2]) / 2.0;
+        }
+        else return arr[n/2];
+    }
+};
